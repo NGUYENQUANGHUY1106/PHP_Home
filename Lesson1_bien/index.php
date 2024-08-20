a259d50f107edf00fd153206577b6ef6531249ea
NGUYENQUANGHUY1106 <nguyenquanghuy110605@gmail.com>
2024-08-20 11:09:54 +0700

    Biến Trong PHP

---
+++ Lesson1_bien/index.php
@@ -0,0 +1,32 @@
+<?php
+  
+  $huy = 'Nguyễn Quang Huy';
+  $bien_so_1 = 11 ;
+  
+  //quy chuẩn đặt tên biến
+
+  $bienSoMot = 12.5;
+
+  // xuất dữ liệu 
+
+//   echo 'Quang Huy 11 <br>';
+//   echo $huy  ;
+  
+  // comment 1 dòng 
+   /* commment nhiều dòng */
+
+
+ /*  Var_dump ($opp) --> trả về kiểu dữ liệu + giá trị 
+   */
+   var_dump($bienSoMot) ;
+   echo "<br>";
+   var_dump($bien_so_1);
+   echo "<br>";
+
+//    Print_r($opp) -> dung để in ra mảng và đối tượng .
+
+$arr = [1,2,3,4];
+
+print_r($arr);
+
+?>
